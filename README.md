5ae1860fd52ebb0e2626144dbec40630080397d2
Kleach112 <s556330@nwmissouri.edu>
2023-09-23 15:43:34 -0500

    Add files to repo

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # streaming-05-smart-smoker-KimLeach
+
+This project demonstrates the use of RabbitMQ to stream temperature readings from a smart smoker. The data is read from a CSV file, `smoker-temps.csv`, which contains temperature readings of the smoker and two types of food over time. Each reading is sent as a message to specific RabbitMQ queues.
+
+## Features
+
+- Reads temperature data from a CSV file.
+- Streams data to RabbitMQ queues with a delay to simulate real-time streaming.
+- Sends smoker temperature readings to "01-smoker" queue.
+- Sends Food A temperature readings to "02-food-A" queue.
+- Sends Food B temperature readings to "03-food-B" queue.
+
+## Prerequisites
+
+- RabbitMQ server running on `localhost`.
+- `pika` Python package installed.
+
+## Setup and Usage
+
+1. Ensure RabbitMQ server is running.
+2. Install the required Python packages.
+3. Run the emitter_smoker_temps.py script
+4. Monitor the RabbitMQ queues using the RabbitMQ Admin site
+
+## Modifying the Emitter
+
+- The emitter script, emitter_smoker_temps.py, can be modified to read data from different CSV files or to send data to different RabbitMQ queues. Refer to the comments in the script for guidance.
+
+## Acknowledgements
+
+- The project structure and approach are based on the lessons and examples from Module 4 and Module 5.
+
+## Screenshots
+
+### Emitter in Visual Studio Code
+![Emitter in VS Code](./Emitter_VS_Code.png)
+
+### RabbitMQ Queue
+![RabbitMQ Queue](./Rabbit_Queue.png)
+
+
+
---
+++ emitter_smoker_temps.py
@@ -0,0 +1,91 @@
+"""
+Module 5 - Smart Smoker Emitter
+
+Modifications Summary:
+----------------------
+1. Updated the CSV file reference to point to 'smoker-temps.csv' instead of 'tasks.csv'.
+2. Modified the main execution block to:
+   a. Read temperature readings from the CSV file, specifically extracting smoker temperature, Food A temperature, and Food B temperature.
+   b. Loop through these readings and send them to their respective RabbitMQ queues, namely "01-smoker", "02-food-A", and "03-food-B".
+   c. Introduced a consistent delay of 30 seconds between reading and sending each set of temperatures to the queues, as specified in the assignment.
+3. Removed redundant code sections and imports to streamline the file.
+
+
+Author: Kim Leach
+Date: 09/23/2023
+
+"""
+
+# Import necessary libraries
+import pika
+import sys
+import webbrowser
+import csv
+import time
+
+# Define the CSV file as a variable
+csv_file = 'smoker-temps.csv'
+
+def offer_rabbitmq_admin_site():
+    """Offer to open the RabbitMQ Admin website"""
+    ans = input("Would you like to monitor RabbitMQ queues? y or n ")
+    print()
+    if ans.lower() == "y":
+        webbrowser.open_new("http://localhost:15672/#/queues")
+        print()
+
+def send_message(host: str, queue_name: str, message: str):
+    """
+    Creates and sends a message to the queue each execution.
+    This process runs and finishes.
+
+    Parameters:
+        host (str): the host name or IP address of the RabbitMQ server
+        queue_name (str): the name of the queue
+        message (str): the message to be sent to the queue
+    """
+
+    try:
+        # create a blocking connection to the RabbitMQ server
+        conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+        # use the connection to create a communication channel
+        ch = conn.channel()
+        # use the channel to declare a durable queue
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
+        # messages will not be deleted until the consumer acknowledges
+        ch.queue_declare(queue=queue_name, durable=True)
+        # use the channel to publish a message to the queue
+        # every message passes through an exchange
+        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
+        # print a message to the console for the user
+        print(f" [x] Sent {message}")
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error: Connection to RabbitMQ server failed: {e}")
+        sys.exit(1)
+    finally:
+        # close the connection to the server
+        conn.close()
+
+if __name__ == "__main__":
+    # ask the user if they'd like to open the RabbitMQ Admin site
+    offer_rabbitmq_admin_site()
+
+    # Read temperature readings from the CSV file
+    readings = []
+    with open(csv_file, mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)  # skip the header
+        for row in reader:
+            readings.append((row[1], row[2], row[3]))  # Extract smoker, Food A, and Food B temperatures
+
+    # Loop through the readings and send them to the respective queues with a delay of 30 seconds
+    for smoker_temp, food_a_temp, food_b_temp in readings:
+        send_message("localhost", "01-smoker", smoker_temp)
+        send_message("localhost", "02-food-A", food_a_temp)
+        send_message("localhost", "03-food-B", food_b_temp)
+        
+        # Delay for 30 seconds between readings as specified in the assignment
+        time.sleep(30)
+        
+print("All temperature readings from the CSV have been sent to the RabbitMQ queues!")
